f4cd7ec388004bf3c529607b8ad367a67245b6bd
PabloEdibaldo <81131909+PabloEdibaldo@users.noreply.github.com>
2024-05-30 18:50:46 -0600

    cambio cambio a text campo Message

--- src/main/java/com/User/User/models/MessageTwilio.java
+++ src/main/java/com/User/User/models/MessageTwilio.java
@@ -15,6 +15,7 @@ public class MessageTwilio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(columnDefinition="TEXT")
     private int caseMessage;
     private String messageTemplate;
 
